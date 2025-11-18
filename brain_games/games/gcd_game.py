7adb45da06878e2e2ad8627a2ff831ef71000b21
Mariia Merkel <mariia_merkel@mail.ru>
2025-11-18 07:11:58 +0300

    add third game

---
+++ brain_games/games/gcd_game.py
@@ -0,0 +1,29 @@
+import random
+import math
+
+
+def main():
+    print("Welcome to the Brain Games!")
+
+
+OPERATORS = ('+', '-', '*', '/', '%')
+
+MAX_TRIES = 3
+RULES = "Calculate the greatest common divided and input result."
+
+
+def check_answer(question: str, answer: str) -> bool:
+    calculation = calculation_of_excperation(question)
+    print(f"calculation >>> {calculation}")
+    return calculation == int(answer)
+
+
+def get_question():
+    return str(f"{random.randint(1, 100)} {random.randint(1, 100)}")
+
+def calculation_of_excperation(question:str) -> int:
+    first, second = question.split(' ')
+    return math.gcd(int(first), int(second))
+
+if __name__ == "__main__":
+    main()
