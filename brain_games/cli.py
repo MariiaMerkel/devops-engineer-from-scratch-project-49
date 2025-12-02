5fa0a7e46dc0b0ef4ed573e269540ea38ae8ce75
Mariia Merkel <mariia_merkel@mail.ru>
2025-12-02 05:47:02 +0300

    fix: welcome

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,7 +2,7 @@ import prompt
 
 
 def welcome_user():
-    name = prompt.string("What is your name? ")
-    city = prompt.string("Where are you from? ")
+    name = prompt.string("May I have your name?")
+    city = prompt.string("Where are you from?")
     print(f"Hello, {name} from {city} !")
     return name, city
