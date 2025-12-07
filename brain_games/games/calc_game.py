d3a8eb7be04393b80d3d8e87980b07531b28a5f9
Mariia Merkel <mariia_merkel@mail.ru>
2025-12-07 09:48:16 +0300

    fix: getting question

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -6,16 +6,10 @@ def main():
     print("Welcome to the Brain Games!")
 
 
-OPERATORS = ('+', '-', '*', '/', '%')
+OPERATORS = ('+', '-', '*')
 
 MAX_TRIES = 3
 RULES = "What is the result of the expression?"
-
-
-def check_answer(question: str, answer: str) -> bool:
-    calculation = calculation_of_expression(question)
-    return calculation == int(answer)
-
 
 def get_question():
     operator = random.choice(OPERATORS)
@@ -34,21 +28,6 @@ def get_question():
             correct_answer = int(first * second)
 
     return str(f"{first} {operator} {second}"), correct_answer
-
-def calculation_of_expression(question:str) -> int:
-    first, operator, second = question.split(' ')
-
-    match operator:
-        case '+':
-            return int(first) + int(second)
-        case '-':
-            return int(first) - int(second)
-        case '*':
-            return int(first) * int(second)
-        case '/':
-            return int(first) / int(second)
-        case '%':
-            return int(first) % int(second)
 
 if __name__ == "__main__":
     main()
